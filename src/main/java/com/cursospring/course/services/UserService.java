810dcca857660d7d81484eb66f04fa7a1832a18e
vfls <vitorfelipe.s@hotmail.com>
2023-12-27 12:26:15 -0300

    Exception handling - delete

--- src/main/java/com/cursospring/course/services/UserService.java
+++ src/main/java/com/cursospring/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.cursospring.course.entities.User;
 import com.cursospring.course.repositories.UserRepository;
+import com.cursospring.course.services.exceptions.DatabaseException;
 import com.cursospring.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -15,19 +18,27 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
+
 	public User findById(Long id) {
-		Optional <User> obj = repository.findById(id);
+		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
